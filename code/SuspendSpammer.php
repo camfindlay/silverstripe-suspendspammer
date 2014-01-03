a917d7824c086a0c5b8d2e81353d34b818f3b4fc
Cam Findlay <cam@silverstripe.com>
2014-01-03 23:28:49 +1300

    Email notifications for suspected spammer registrations

--- code/SuspendSpammer.php
+++ code/SuspendSpammer.php
@@ -12,6 +12,12 @@ class SuspendSpammer extends DataObjectDecorator {
 	//Commonly filled in fields in the forum module to check.
 	public static $fields_to_check = array( 'Occupation', 'Company' );
 
+	//enable emails to be send to admin on suspected spammer registrations.
+	public static $enable_email = false;
+
+	//Email address to send suspended registrations to for review.
+	public static $email_to;
+
 
 	/**
 	 * Decorate the Member object to stop the writing of registrations is the user is suspected of being a spammer.
@@ -19,8 +25,8 @@ class SuspendSpammer extends DataObjectDecorator {
 	public function onBeforeWrite() {
 		parent::onBeforeWrite();
 
-		$spam_needles = DataObject::get('SuspendSpammerKeyword');
-		if($spam_needles){
+		$spam_needles = DataObject::get( 'SuspendSpammerKeyword' );
+		if ( $spam_needles ) {
 			$spam_needles = $spam_needles->map();
 		} else {
 			$spam_needles = array();
@@ -30,7 +36,26 @@ class SuspendSpammer extends DataObjectDecorator {
 		if ( 0 < count( array_intersect( $this->spamHaystack() , $spam_needles ) ) ) {
 			$this->owner->SuspendedUntil = date( 'Y-m-d', time() + strtotime( '+ 100 years' ) );
 
-			//@todo - Email the admin to let them know to check the registration and re-enable if it was a false positive.
+			//Email the admin to let them know to check the registration and re-enable if it was a false positive.
+			if ( self::$enable_email ) {
+				
+				$from = Email::getAdminEmail();
+				
+				$to = self::$email_to;
+				
+				$subject = "Suspected spammer registration suspended.";
+
+				$body = "<h1>Suspected Spammer Details</h1>
+				<ul>
+				<li>Email: " . $this->owner->Email . "</li>";
+				foreach ( self::$fields_to_check as $field ) {
+					$body .= "<li>" . $field . ": " . $this->owner->$field . "</li>";
+				}
+				$body .= "</ul>";
+
+				$email = new Email( $from, $to, $subject, $body );
+				$email->send();
+			}
 		}
 	}
 
@@ -38,13 +63,14 @@ class SuspendSpammer extends DataObjectDecorator {
 
 	/**
 	 * Creates an array to hold common used fields upon registration which would denote Member as a spammer.
+	 *
 	 * @retrun array haystack of words to check against known spam related words.
 	 */
 	private function spamHaystack() {
-		
+
 		$spamstring = '';
-		
-		foreach(self::$fields_to_check as $field){
+
+		foreach ( self::$fields_to_check as $field ) {
 
 			$spamstring .= $this->owner->$field . ' ';
 
@@ -53,7 +79,7 @@ class SuspendSpammer extends DataObjectDecorator {
 		$spam_haystack = array_map( 'strtolower', explode( ' ', $spamstring ) );
 
 		return $spam_haystack;
-		
+
 	}
 
 }
